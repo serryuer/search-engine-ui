5a9f7b193f609da0ff87646e627a0026789febee
serryuer <yujunshuai@iie.ac.cn>
2019-05-02 19:07:51 +0800

    add sort by time

--- query.py
+++ query.py
@@ -2,6 +2,8 @@ from whoosh import index
 from whoosh.qparser import QueryParser
 from whoosh.qparser.dateparse import DateParserPlugin
 
+from whoosh.sorting import FieldFacet
+
 from config import config
 
 
@@ -17,9 +19,19 @@ class Query(object):
         self.qp.add_plugin(DateParserPlugin())
 
     def query_page(self, term, page_num, page_len, sort_type):
+
         with self.ix.searcher() as searcher:
-            results = searcher.search_page(self.qp.parse(
-                term), pagenum=page_num, pagelen=page_len)
+            if sort_type == 1:# default sorted
+                results = searcher.search_page(self.qp.parse(
+                    term), pagenum=page_num, pagelen=page_len)
+            if sort_type == 2:# sorted by publish time
+                publish_time = FieldFacet("publish_time", reverse=True)
+                results = searcher.search_page(self.qp.parse(
+                    term), pagenum=page_num, pagelen=page_len, sortedby=publish_time)
+            if sort_type == 3:# sorted by custom hot value
+                publish_time = FieldFacet("publish_time", reverse=True)
+                results = searcher.search_page(self.qp.parse(
+                    term), pagenum=page_num, pagelen=page_len, sortedby=publish_time)
             data = {}
             data["total"] = results.total
             result_list = []
@@ -46,7 +58,8 @@ class Query(object):
         cut_desc = description[:160]
         i = 160
         letter = description[i]
-        while not (letter == ',' or letter == '，' or letter == '.' or letter == '。'):
+        length = len(description)
+        while i < length - 1 and not (letter == ',' or letter == '，' or letter == '.' or letter == '。'):
             cut_desc += letter
             i = i + 1
             letter = description[i]
@@ -77,7 +90,8 @@ class Query(object):
     def get_recommend_query(self, term):
         recom_query = []
         with self.ix.searcher() as searcher:
-            results = searcher.search_page(self.qp.parse(u"推荐"), pagenum=1, pagelen=10)
+            results = searcher.search_page(
+                self.qp.parse(u"推荐"), pagenum=1, pagelen=10)
             for result in results:
                 item = {}
                 item['term'] = result['title']
@@ -88,4 +102,4 @@ class Query(object):
 if __name__ == '__main__':
     query = Query()
     # print(query.query("测试", 1, 10))
-    print(query.get_recommend_query("测试"))
+    print(query.query_page(u"测试", 1, 10, 1))
