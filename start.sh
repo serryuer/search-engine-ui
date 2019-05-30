fd532d35d54712b0870cd73c64f7cc667f5acc90
zZhouLi <45137711+zZhouLi@users.noreply.github.com>
2019-05-30 16:33:24 +0800

    fix encoding problem

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export FLASK_APP=run.py
 log=`date +%Y-%m-%d-%H-%M-%S`.log
-nohup /root/anaconda3/envs/ir/bin/python run.py 2>&1 $log &
+PYTHONIOENCODING=utf-8 nohup /root/anaconda3/envs/ir/bin/python run.py 2>&1 $log &
